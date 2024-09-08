548c3a8924d8a36f39ce0a3792373516aa9e20e8
Тимофей <shvedov.tim30@gmail.com>
2024-09-08 18:57:33 +0300

    +

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from telebot.types import KeyboardButton, ReplyKeyboardMarkup
 from telebot.types import Message
 from dodo import BOT_ID
 from pprint import pprint
-
+import requests
 import geonamescache
 
 gc = geonamescache.GeonamesCache()
@@ -29,6 +29,10 @@ def soobsh(message: Message):
     knopki.add(knopk)
     # telbot.send_message(message.from_user.id,"вас выследили",reply_markup=knopki)
     telbot.send_sticker(message.from_user.id, "CAACAgIAAxkBAAEMM81mU1B_M45YswKX4Lt-5PM9uaktQAACAQADwDZPExguczCrPy1RNQQ")
+@telbot.message_handler(commands=["clear"])
+def chistka(message: Message):
+    memory.clear()
+
 
 
 @telbot.message_handler(content_types=["text"])
@@ -45,17 +49,22 @@ def soobsh(message: Message):
         telbot.send_message(message.from_user.id,"Такой город уже существует")
         return
     if len(memory)>0:
-        if memory[-1][-1]!=city_player[0]:
+        member = memory[-1][-1]
+        if memory[-1][-1] in ["ь", "ъ", "ы","й"]:
+            member=memory[-1][-2]
+        if member!=city_player[0]:
             telbot.send_message(message.from_user.id, "Вам на другую букву")
             return
     memory.append(city_player)
+    lat,lon = found_city[0]["latitude"],found_city[0]["longitude"]
     last_char = city_player[-1]
 
-    if city_player[-1] in ["ь", "ъ", "ы"]:
+    if city_player[-1] in ["ь", "ъ", "ы","й"]:
         last_char = city_player[-2]
     telbot.send_message(message.from_user.id, f"Мне на букву {last_char}")
     botcity = poisk(word=last_char)
     memory.append(botcity)
+
     telbot.send_message(message.from_user.id, botcity)
     print((memory))
 
@@ -67,10 +76,23 @@ def poisk(word):
             if y and y[0] == word.upper() and y not in memory:
                 return y
 
+def sience():
+    fff = "http://api.airvisual.com/v2/nearest_city?lat=59.453791&lon=32.030708&key=e5bcfe8d-df20-4d0a-9954-5845c6396e0e"
+    otvet = requests.get(fff)
+    pprint(otvet.json())
+    life = otvet.json()["data"]["current"]["pollution"]["aqicn"]
+    gras = otvet.json()["data"]["current"]["weather"]["ts"]
+    ic = otvet.json()["data"]["current"]["weather"]["ic"]
+    tp = otvet.json()["data"]["current"]["weather"]["tp"]
+
 
 telbot.polling()
 
 """
-загуглить: "как получить данные из википедии на пайтоне"
-посмотреть гайд на первых двух сайтах и сделать тоже самое, просто достать что-либо из википедии при помощи пайтона) это нам понадобится ;)
+
+. Внутрь функции science передавать два параметра: lat, lon (широта долгота).
+2. Внутри функции есть ссылка, сделать из неё F строчку и подставить два аргумента (lat/lon) в нужные места
+3. В той же функции поставить просто принт с 3 переменными (ic, life...)
+4. Подумать, где нужно вызвать функцию и передать в неё ДВА параметра lat, lon.
+
 """
